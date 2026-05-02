f492a9969a381f5e7ef84a40a26aa9367b0cc3b0
Carlos A <cayala.w@gmail.com>
2026-05-02 00:13:22 -0500

    rspec test for model and controller

--- spec/models/job_application_spec.rb
+++ spec/models/job_application_spec.rb
@@ -15,5 +15,35 @@
 require 'rails_helper'
 
 RSpec.describe JobApplication, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it 'is invalid without an application date' do
+      job_application = JobApplication.new(application_date: nil)
+      job_application.valid?
+      expect(job_application.errors[:application_date]).to include("can't be blank")
+    end
+
+    it 'is invalid without a position' do
+      job_application = JobApplication.new(position: nil)
+      job_application.valid?
+      expect(job_application.errors[:position]).to include("can't be blank")
+    end
+
+    it 'is invalid without a company' do
+      job_application = JobApplication.new(company: nil)
+      job_application.valid?
+      expect(job_application.errors[:company]).to include("can't be blank")
+    end
+
+    it 'is invalid without a platform' do
+      job_application = JobApplication.new(platform: nil)
+      job_application.valid?
+      expect(job_application.errors[:platform]).to include("can't be blank")
+    end
+
+    it 'has default state as draft' do
+      job_application = JobApplication.new()
+      expect(job_application.draft?).to be true
+    end
+  end
 end
--- spec/requests/job_application_spec.rb
+++ spec/requests/job_application_spec.rb
@@ -1,8 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "JobApplications", type: :request do
+  let(:valid_attributes) { { application_date: Date.today, position: Faker::Company.profession,
+                            company: Faker::Company.name, platform: "LinkedIn", state: :draft } }
+  let(:invalid_attributes) { { application_date: Date.yesterday, position: nil,
+                               company: nil, platform: nil } }
+
   describe "GET /index" do
-    get job_application_index_path
-    expect(response).to have_http_status(:ok)
+    it 'returns a successful response' do
+      get job_applications_path
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "POST /create" do
+    it 'returns a successful response' do
+      expect { post job_applications_path, params: { post: valid_attributes } }.to change(JobApplication, :count).by(1)
+      expect(response).to redirect_to(job_application_path(JobApplication.last))
+    end
   end
 end
